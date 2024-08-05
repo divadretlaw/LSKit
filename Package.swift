c5d2ad01c0210619724be94a6ee0909364424336
David Walter <hello@davidwalter.at>
2024-08-05 20:29:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,8 @@ let package = Package(
     name: "LSKit",
     platforms: [
         .macOS(.v12),
-        .iOS(.v14)
+        .iOS(.v15),
+        .visionOS(.v1)
     ],
     products: [
         .library(
